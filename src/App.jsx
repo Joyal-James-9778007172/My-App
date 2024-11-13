786f5478e2fda72bfbd6bd55b9d817e3404b17ad
joyal james <joyaljames58@gmail.com>
2024-11-13 18:28:45 +0530

    mesaage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WELCOME</h1>
+      <P>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates impedit accusantium provident necessitatibus laboriosam veritatis architecto soluta sit, porro ab unde, cumque quas doloribus, nihil obcaecati possimus earum dolores quisquam.
+      Ea reiciendis corporis dicta debitis tempora fugit veniam, consequatur hic voluptate distinctio similique dolorem, natus unde et aperiam totam id, accusantium deserunt iste dignissimos obcaecati placeat! Atque ducimus dolore maxime.</P>
+      
+    </div>
+  );
+}
+
+export default App;
